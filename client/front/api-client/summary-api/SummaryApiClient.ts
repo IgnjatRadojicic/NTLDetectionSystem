6af711ca20407c97cc29d753570cea400973a966
Ignjat Radojicic <ignjatradojicic@gmail.com>
2026-05-08 04:35:30 +0200

    Add static summary data and client switch

--- client/front/api-client/summary-api/SummaryApiClient.ts
+++ client/front/api-client/summary-api/SummaryApiClient.ts
@@ -6,24 +6,19 @@ import {
 
 export class SummaryApiClient implements ISummaryApiClient {
   private readonly baseUrl: string;
+  private readonly useStaticData: boolean;
 
   constructor(baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://localhost:5601") {
     this.baseUrl = baseUrl.replace(/\/$/, "");
+    this.useStaticData = process.env.NEXT_PUBLIC_USE_STATIC_DATA === "true";
   }
 
   async getSummary(params?: GetSummaryParams): Promise<NtlSummaryResponse> {
-    const query = new URLSearchParams();
+    const url = this.useStaticData
+      ? "/data/summary.json"
+      : this.buildLiveUrl(params);
 
-    if (params?.from) {
-      query.set("from", params.from);
-    }
-
-    if (params?.to) {
-      query.set("to", params.to);
-    }
-
-    const querySuffix = query.toString() ? `?${query.toString()}` : "";
-    const response = await fetch(`${this.baseUrl}/api/Summary${querySuffix}`, {
+    const response = await fetch(url, {
       method: "GET",
       headers: {
         Accept: "application/json",
@@ -38,5 +33,20 @@ export class SummaryApiClient implements ISummaryApiClient {
     }
 
     return (await response.json()) as NtlSummaryResponse;
+  }
+
+  private buildLiveUrl(params?: GetSummaryParams): string {
+    const query = new URLSearchParams();
+
+    if (params?.from) {
+      query.set("from", params.from);
+    }
+
+    if (params?.to) {
+      query.set("to", params.to);
+    }
+
+    const querySuffix = query.toString() ? `?${query.toString()}` : "";
+    return `${this.baseUrl}/api/Summary${querySuffix}`;
   }
 }
